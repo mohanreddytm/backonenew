f05e36c28e806610c5b47e2bbbe1d72c82e9905f
mohanreddytm <tamminenimohanreddy1@gmail.com>
2025-01-21 16:07:56 +0530

    Initial commit

---
+++ frontone/src/App.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+
+import "./App.css";
+
+function App() {
+
+
+  return (
+    <div>
+      <h1>Fullstack App</h1>
+      <p className="one">mohan</p>
+    </div>
+  );
+}
+
+export default App;
